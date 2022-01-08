936d380872d8d0cf69bb23cae3b13c5705793dab
Arushi Somani <arushisomani@berkeley.edu>
2022-01-09 01:14:20 +0530

    echoing text input

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,12 @@
 def main():
-    pass
+    while True:
+        try:
+            text = input('calc> ')
+        except EOFError:
+            break
+        if not text:
+            continue
+        print(text)
 
 if __name__ == '__main__':
     main()
