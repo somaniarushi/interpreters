8c2c669084d8f429a3f552683b79b4ad91cd4551
Arushi Somani <arushisomani@berkeley.edu>
2022-01-09 01:16:33 +0530

    adding interpreter class

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,21 @@
+class Interpreter:
+    def __init__(self, text):
+        '''
+        Accepts a string input from the client as text,
+        and maintains an index into text and the
+        current token instance.
+        '''
+        self.text = text
+        self.pos = 0
+        self.curent_token = None
+
+    def expr(self):
+        '''
+        Returns the text and returns the expression.
+        '''
+        return self.text
+
+
 def main():
     while True:
         try:
@@ -6,7 +24,9 @@ def main():
             break
         if not text:
             continue
-        print(text)
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
 
 if __name__ == '__main__':
     main()
