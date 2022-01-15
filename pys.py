5921b68276182a48b65c7a72d38f7e610088c241
Arushi Somani <arushisomani@berkeley.edu>
2022-01-15 23:47:19 +0530

    adding visualization support for simple statements

--- pys.py
+++ pys.py
@@ -2,17 +2,31 @@ from constants import *
 from lexers import Lexer
 from parsers import Parser
 from interpreters import Interpreter
+from visualizer import ASTVisualizer
+from viztools import VizParser, VizLexer
 
 
 def main():
     import sys
     text = open(sys.argv[1], 'r').read()
+    visualize = False
+    if len(sys.argv) > 2:
+        if sys.argv[2] == '--visualize':
+            visualize = True
 
-    lexer = Lexer(text)
-    parser = Parser(lexer)
-    interpreter = Interpreter(parser)
-    result = interpreter.interpret()
-    print(interpreter.GLOBAL_SCOPE)
+
+
+    if visualize:
+        lexer = VizLexer(text)
+        parser = VizParser(lexer)
+        visualizer = ASTVisualizer(parser)
+        visualizer.visualize()
+    else:
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
+        result = interpreter.interpret()
+        print(interpreter.GLOBAL_SCOPE)
 
     # while True:
     #     try:
