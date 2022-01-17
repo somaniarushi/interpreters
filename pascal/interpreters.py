971408440771c103f97e74d69c6385cfdadaa843
Arushi Somani <arushisomani@berkeley.edu>
2022-01-18 00:22:45 +0530

    adding symbol table buildeR

--- pascal/interpreters.py
+++ pascal/interpreters.py
@@ -1,6 +1,7 @@
 from ctypes.wintypes import FLOAT
 from pascal.constants import *
 from pascal.parsers import Block
+from pascal.symbols import SymbolTable, VarSymbol
 
 class NodeVisitor:
     def visit(self, node):
@@ -54,7 +55,6 @@ class Interpreter(NodeVisitor):
         else:
             return val
 
-
     def visit_BinOp(self, node):
         if node.op.type == PLUS:
             return self.visit(node.left) + self.visit(node.right)
@@ -76,3 +76,56 @@ class Interpreter(NodeVisitor):
 
     def visit_Num(self, node):
         return node.value
+
+class SymbolTableBuilder(NodeVisitor):
+    '''
+    Traverses through the AST, adding stuff to the
+    symbol table as it goes
+    '''
+    def __init__(self):
+        self.symtab = SymbolTable()
+
+    def visit_Program(self, node):
+        self.visit(node.block)
+
+    def visit_Block(self, node):
+        for declaration in node.declarations:
+            self.visit(declaration)
+        self.visit(node.compound_statement)
+
+    def visit_VarDecl(self, node):
+        type_name = node.type_node.value
+        type_symbol = self.symtab.lookup(type_name)
+        if not type_symbol:
+            raise Exception(f'Referenced type {type_name} does not exist')
+        var_name = node.var_node.value
+        var_symbol = VarSymbol(var_name, type_symbol)
+        self.symtab.define(var_symbol)
+
+    def visit_Assign(self, node):
+        var_name = node.left.value
+        var_symbol = self.symtab.lookup(var_name)
+        if var_symbol is None:
+            raise Exception(f'variable {var_name} not defined.')
+
+        self.visit(node.right)
+
+    def visit_Var(self, node):
+        var_name = node.value
+        var_symbol = self.symtab.lookup(var_name)
+        if var_symbol is None:
+            raise Exception(f'Variable {var_name} not defined.')
+
+    def visit_NoOp(self, node):
+        pass
+
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_UnaryOp(self, node):
+        self.visit(node.expr)
+
+    def visit_Num(self, node):
+        pass
+
